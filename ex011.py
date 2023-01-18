9e36cd1b65776326416e6cdbd7b6d984728d01a5
crija <crijacarla@gmail.com>
2023-01-18 19:48:57 -0300

    Initial commit

---
+++ ex011.py
@@ -0,0 +1,16 @@
+#crie um programa que leia quanto dinheiro (real) o usuario quer converter e para qual moeda :
+
+
+real = float(input('Digite o valor em (real) que voce quer converter: R$'))
+converter = input('Para qual dessas moedas voce deseja converter: (dólar, euro ou iene)? ')
+dólar = real / 5.16
+euro = real / 5.57
+iene = real / 0.040
+if converter == 'dólar':
+    print('US${:.2f}'.format(dólar))
+if converter == 'euro':
+    print('£{:.2f}'.format(euro))
+if converter == 'iene':
+    print('¥{:.2f}'.format(iene))
+else:
+    print('Moeda não identificada')
