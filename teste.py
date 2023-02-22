7b3e66cda38994264c7da1f8c75759c2b88e9975
crija <crijacarla@gmail.com>
2023-02-21 23:13:14 -0300

    new file

---
+++ teste.py
@@ -0,0 +1,13 @@
+nome = str(input('Digite seu nome: '))
+print('')
+print('Loading...')
+print('')
+print('FELIZ DOIS MESES DE NAMORO, {} ^-^'.format(nome))
+print('''
+                    -  .   .   -
+                 -       .       -
+                 -   i love you  -
+                   -           -
+                      -      -
+                         .           ''')
+                         
