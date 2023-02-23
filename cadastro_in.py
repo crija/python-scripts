29ca4602fc377d6fb23ab46101484fedddac11e6
crija <crijacarla@gmail.com>
2023-02-22 21:21:58 -0300

    new registry

---
+++ cadastro_in.py
@@ -0,0 +1,25 @@
+
+print('*Indica item obrigatório')
+
+print('')
+
+print('Nome*')
+nome = str(input('- '))
+
+print('_' *40)
+
+print('Sobrenome*')
+sobrenome = str(input('- '))
+
+print('_' *40)
+
+print('Nome adicional')
+n_a = str(input('- '))
+
+print('_' *40)
+
+print('Inserir pronomes personalizados')
+print('Informe as pessoas como elas devem se referir a você.')
+p_p = input('-  ')
+
+print('_' *40)
