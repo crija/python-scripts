ade19b12aae26dae6f88b2e823b225e0d54f4dde
crija <crijacarla@gmail.com>
2023-02-24 17:15:17 -0300

    Add new shape

--- ex034.py
+++ ex034.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia uma frase pelo teclado e mostre:
 #Quantas vezes aparece a letra A:
 
-frase = str(input('Digite uma frase: '))
+'''frase = str(input('Digite uma frase: '))
 
 ndv = 0
 
@@ -9,4 +9,8 @@ for letter in frase :
     if letter == 'a' or letter == 'A' :
         ndv = ndv + 1
 
-print('A letra A repete {}:'.format(ndv))
+print('A letra A repete {}:'.format(ndv))'''
+
+frase = str(input('Digite uma frase: ')).upper()
+
+print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
