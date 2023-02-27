5a5dc2448fa65149df8bdaa349d9c941b0a3df92
crija <crijacarla@gmail.com>
2023-02-27 11:25:36 -0300

    String analysis

---
+++ ex036.py
@@ -0,0 +1,12 @@
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o últim nome separadamente.
+
+print('Digite seu nome completo:')
+nome = str(input('')).strip()
+
+n = nome.split()
+
+print('Prazer em te conhecer!')
+
+print('Seu primeiro nome é {}'.format(n[0]))
+
+print('Seu último nome é {}'.format(n[len(n)-1]))
