28d496a7ebc085a133fc29945b0e24faca2c2683
crija <crijacarla@gmail.com>
2023-03-06 18:50:37 -0300

    Improve the code ex042.py

--- ex042.py
+++ ex042.py
@@ -2,19 +2,10 @@
 
 ano =  int(input('Em qual ano você está? '))
 
-a = ano / 400
-res1 = a % b
-
-c = ano / 4
-res2 = ano % 4
-
-d = ano / 100
-res3 = ano % 100
-
-if res1 == 0 or res2 == 0 and res3 != 0:
-
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    
     print('{} é um ano bisseto.'.format(ano))
 
 else:
-    
+
     print('{} não é um ano bissexto.'.format(ano))
