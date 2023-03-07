63a737263691fea253d6487d9a32ddfa5218b672
crija <crijacarla@gmail.com>
2023-03-07 11:58:43 -0300

    Add colors

--- ex045.py
+++ ex045.py
@@ -1,16 +1,16 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 print('-='*20)
 
-print('Analisandor de triângulo')
+print('\033[35m Analisandor de \033 \033[1;35m TRIÂNGULO \033[m .')
 
 print('-='*20)
 
-r1 = float(input('Primeiro segmento: '))
-r2 = float(input('Segundo segmento: '))
-r3 = float(input('Terceiro segmento: '))
+r1 = float(input('\033[1;37m Primeiro segmento: \033[m '))
+r2 = float(input('\033[1;37m Segundo segmento: \033[m '))
+r3 = float(input('\033[1;37m Terceiro segmento: \033[m '))
 
 if r1 < r2 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos acima PODEM FORMAR triângulo!')
+    print('Os segmentos acima \033[34m PODEM FORMAR \033[m triângulo!')
 
 else:
-    print('Os segmento acima NÃO PODEM FORMAR triângulo.')
+    print('Os segmento acima \033[31m NÃO PODEM FORMAR \033[m triângulo.')
