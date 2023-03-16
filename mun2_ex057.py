de988cc4b2ff4fd0a2002f23ef03f0ecd6ce30ca
crija <crijacarla@gmail.com>
2023-03-16 17:13:51 -0300

    Add even numbers using repeating loops

---
+++ mun2_ex057.py
@@ -0,0 +1,8 @@
+#Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50:
+
+print('Todos os números pares de 1 a 50.')
+
+for p in range (0, 51, 2):
+    print(p)
+
+print('FIM!')
