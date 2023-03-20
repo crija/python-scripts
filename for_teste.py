d04f1216b0d86dbc4eb0bcaad54bca3c15b2963d
crija <crijacarla@gmail.com>
2023-03-20 19:08:27 -0300

    Add trial and error

---
+++ for_teste.py
@@ -0,0 +1,28 @@
+#Treino:
+
+'''for c in range(1, 4):
+    print('valor de c: ' + str(c))'''
+
+'''for c in['banana', 'maçã', 'goiaba', 'manga']:
+    print('Hoje pela manhã eu comi:' + c)'''
+
+
+'''for c in range(50, 0, -5):
+    print(c)'''
+
+
+'''for c in range(1, 8,):
+    if c < 4:
+        print('O número {} < 4'.format(c))
+
+    elif c > 5:
+        print('O número {} > 4'.format(c))'''
+
+
+'''for c in['Ana', 'Bia', 'Carla', 'Deise']:
+    if c == 'Ana':
+        print(c)'''
+
+for c in[ 'Ana', 'Alan' 'Alana','Bianca', 'Bruna', 'Bruno','Carlos','Daniel', 'Deise', 'Dionatan']:
+    if c == 'Ana':
+        print(c)
