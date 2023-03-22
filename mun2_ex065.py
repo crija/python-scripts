d5bb90ca957755a475f267400e244bdb07a61786
crija <crijacarla@gmail.com>
2023-03-22 12:16:58 -0300

    Add new version

--- mun2_ex065.py
+++ mun2_ex065.py
@@ -1,42 +1,43 @@
+
+
+#Escrever um programa que leia a opção desejada do cliente,
+
+import time
+
 print('Remédios ainda em estoque:')
 print('¨'* 15)
 print('''
 Tilenol
 Patanol
-Ecofilm
-camomila
-Paracetamol
 ''')
 print('¨'* 15)
 
-remedio = str(input('Qual das opções você deseja? ')).split()
+remedio = str(input('Qual das opções você deseja? '))
 
-if remedio == 'Tilenol'  or remedio == 'Patanol':
+nome = str(input('Qual seu nome? '))
 
-    print('{} é indicado apenas para pessoas com mais de 18.'.format(remedio))
+idade = int(input('Quantos anos você tem? '))
 
-    idade = int(input('Quantos anos você tem? '))
+# especifique se o remedio escolhido é recomendado para a idade dele ou nao,
+
+if remedio == 'Tilenol' or remedio == 'Patanol':
 
     if idade < 18:
 
         print('{} NÃO é recomendado para menor de idade.'.format(remedio))
 
-elif idade >= 18:
+# se o usuario for menor de idade nao autorize  a compra
 
-    nome = str(input('Qual é seu nome? '))
+        print('A venda de {} não é autorizada para menores de 18 anos'.format(remedio))
+        print('Compareça a loja mais próxima com um adulto responsável para a compra.')
 
-    print('Cliente: {}'.format(nome))
+    elif idade >= 18:
 
-elif remedio != 'Tilenol' or remedio != 'Patanol':
+        print('CARREGANDO INFORMAÇÕES...')
 
-    nome = str(input('Qual é seu nome? '))
+        time.sleep(2)
 
-    print('cliente: {}'.format(nome))
-
-
-
-
-
-
-elif remedio == 'Ecofilm' or remedio == 'Camomila' or remedio == 'Paracetamol':
-    print()
+        print('''
+Nome: {}
+Idade: {}
+Produto: {}'''.format(nome, idade, remedio))
