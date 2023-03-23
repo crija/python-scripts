5a995949c7e7abc99b05032023effedd706006c3
crija <crijacarla@gmail.com>
2023-03-23 18:40:34 -0300

    Add new test

--- for_teste.py
+++ for_teste.py
@@ -23,6 +23,15 @@
     if c == 'Ana':
         print(c)'''
 
-for c in[ 'Ana', 'Alan' 'Alana','Bianca', 'Bruna', 'Bruno','Carlos','Daniel', 'Deise', 'Dionatan']:
+'''for c in[ 'Ana', 'Alan' 'Alana','Bianca', 'Bruna', 'Bruno','Carlos','Daniel', 'Deise', 'Dionatan']:
     if c == 'Ana':
-        print(c)
+        print(c)'''
+
+'''soma = 0
+cont = 0
+for c in range(1, 5):
+    n = int(input('Digite o {} número: '.format(c)))
+    if n % 2 == 0:
+        soma = soma + 1
+        cont = cont + n
+print('A soma dos {} números PARES é {}.'.format(cont, soma))'''
