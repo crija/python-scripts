ec4cc3039df82e9ff68e7281448e335ed5097a73
crija <crijacarla@gmail.com>
2023-03-24 11:40:22 -0300

    Add new mode

--- mun2_ex067.py
+++ mun2_ex067.py
@@ -1,8 +1,32 @@
 #Crie um programa que leia a ano idade de sete pessoas. No final, mostre quantas pessoas ainda são de menor:
-soma = 0
+'''soma = 0
 cont = 0
 for c in range(1, 8):
     idade = int(input('qual a idade da pessoa {}? '.format(c)))
     if idade < 18:
         soma = soma + 1
-print('Das {} pessoas {} são MENORES de idade'.format(c, soma,))
+print('Das {} pessoas {} são MENORES de idade'.format(c, soma,))'''
+
+
+#Modo 2:
+from datetime import date
+
+atual = date.today().year
+
+totmaior = 0
+totmenor = 0
+
+for c in range(1, 8):
+
+    ano = int(input('Qual ano a pessoa {} nasceu? '.format(c)))
+
+    idade = atual - ano
+
+    if idade >= 18:
+
+        totmaior = totmaior + 1
+    else:
+
+        totmenor = totmenor + 1
+
+print('Das {} pessoas {} são menores de idade e {} são maiores de idade'.format(c, totmenor, totmaior))
