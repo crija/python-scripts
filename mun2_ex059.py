97952138a6552db6d1de7df448fd09399cef8973
crija <crijacarla@gmail.com>
2023-04-05 17:51:35 -0300

    Add new file

--- mun2_ex059.py
+++ mun2_ex059.py
@@ -1,10 +1,5 @@
 #Faça a taboada de um número que o usuário escolher, usando o laço de repetição:
 
-'''num = int(input('Digite um número: '))
-for c in range(0, 11):
-    print('{} x {:2} = {:2}'.format(num, c, num*c))'''
-
-
 while True:
 
     num = int(input('Quer saber a tabuada de qual número? '))
