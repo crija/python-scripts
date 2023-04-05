5d3d508177724e66c2c005560de16e79a422d6ae
crija <crijacarla@gmail.com>
2023-04-05 18:36:18 -0300

    Add tuple summary

---
+++ mun3.tuplas.py
@@ -0,0 +1,38 @@
+#VARIAVEIS COMPOSTAS(TUPLAS):
+
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim')
+
+'''print(lanche)
+
+print(lanche[2])
+
+print(lanche[1:])
+
+print(lanche[-1])
+
+len(lanche)
+
+for c in lanche:
+    print(c)
+
+for c in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[c]}')
+                OU
+for c in lanche:
+    print(f'Eu vou comer {c}')
+
+for pos, comida in enumerate (lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+a = (2, 5, 4)
+b = (5, 8, 9, 3)
+c = a + b
+print(f'Os elementos são {c}')
+print(f'Tem {len(c)} elementos')
+print(f'Tem {c.count(5)} elementos com o número 5')
+print(f'A posição do número 8 é a {c.index(8)}')'''
+
+'''del(comida)'''
+
+'''pessoa = ('Ana', 18, 56)
+print(pessoa)'''
