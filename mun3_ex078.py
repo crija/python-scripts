5f51c0158d606afa9d92bf228af0180519d8fb39
crija <crijacarla@gmail.com>
2023-04-06 16:33:52 -0300

    Add number in full

---
+++ mun3_ex078.py
@@ -0,0 +1,15 @@
+#Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até dez
+#Seu programa deverá ler um número pelo teclado(entre 0-10) e mostrá-lo por extenso
+num = 0
+
+extenso = ('ZERO','UM', 'DOIS','TRÊS', 'QUATRO', 'CINCO', 'SEIS', 'SETE', 'OITO', 'NOVE', 'DEZ')
+
+
+while True:
+
+    num = int(input('Digite um número de 0 a 10: '))
+    if num < 10:
+        break
+    print('Tente novamente')
+
+print(f'Você digitou o número {extenso[num]}')
