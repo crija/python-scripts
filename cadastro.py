fc91747cf492f4ae506246a6ba09e3f5e2ee3856
crija <crijacarla@gmail.com>
2023-04-13 22:05:46 -0300

    Add new modified

--- cadastro.py
+++ cadastro.py
@@ -2,17 +2,22 @@
 #Pass.1 [Crie uma string pedindo o nome, idade e gmail:]
 #Pass.2 [Crie uma string confirmando os dados enviados:]
 #Pass.3 [Peça para o usuario confirmar se está certo ou não:]
+cadastro = ''
+while cadastro != 'NAO' and cadastro != 'SIM':
+    nome = input('Qual é seu nome? ')
+    idade = input('Qual é sua idade? ')
+    gmail = input('Qual é seu gmail? ')
 
-nome = input('Qual é seu nome? ')
-idade = input('Qual é sua idade? ')
-gmail = input('Qual é seu gmail? ')
+    print(f'O seu cadastro foi confirmado! Seu nome é: {nome}, voce tem: {idade} anos, seu gmail é: {gmail}.')
 
-print('O seu cadastro foi confirmado! Seu nome é: ' +nome+ ', voce tem: ' +idade+ ' anos, seu gmail é: ' +gmail+ '.')
 
-cadastro = input('O seu cadastro está correto? ')
+    cadastro = input('O seu cadastro está correto? [SIM/NAO]').upper()
 
-if cadastro =='sim':
-    print('Tudo certo!')
-    
-else:
-    print('Reenvie seus dados!')
+    if cadastro =='SIM':
+        print('Tudo certo!')
+
+    elif cadastro == 'NAO':
+        print('Reenvie seus dados!')
+        
+    else:
+        print('Rensponda novamente [SIM/NAO]')
