2cb8928373164fac52a057ac207959372c5e9148
crija <crijacarla@gmail.com>
2023-04-18 21:47:25 -0300

    Add new file

--- teste02.py
+++ teste02.py
@@ -1,4 +1,11 @@
-nome = 'Any'
-idade = 16
-peso = 50
-print(nome, idade, peso)
+meunome = ''
+nome = str(input('Qual seu nome? '))
+print('Que nome bonito.')
+while meunome != 'CARLA':
+    meunome = str(input('Agora divinha meu o nome: ')).upper()
+    if meunome == 'CARLA':
+        print('Isso aí, você acertou!')
+        print(f'Prazer em te conhecer, {nome}!')
+    else:
+        print('Ops...você errou')
+        print('Tente de novo!')
