22efaf4f2df34ccb952e67e18ab899a688d2a323
crija <crijacarla@gmail.com>
2023-04-19 22:43:22 -0300

    Add new modification

--- teste02.py
+++ teste02.py
@@ -1,11 +1,21 @@
 meunome = ''
+
 nome = str(input('Qual seu nome? '))
+
 print('Que nome bonito.')
+
 while meunome != 'CARLA':
+
     meunome = str(input('Agora divinha meu o nome: ')).upper()
+
     if meunome == 'CARLA':
+
         print('Isso aí, você acertou!')
+
         print(f'Prazer em te conhecer, {nome}!')
+
     else:
+
         print('Ops...você errou')
+        
         print('Tente de novo!')
