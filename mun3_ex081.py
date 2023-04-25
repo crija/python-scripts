624f78aff30cb9439d85a1d0c30cda6cf60cc1ce
crija <crijacarla@gmail.com>
2023-04-25 16:43:28 -0300

    Add time

--- mun3_ex081.py
+++ mun3_ex081.py
@@ -1,4 +1,7 @@
 #Crie um programa que tenha uma tupla única com nomes de produtos e seus respectores preços, na sequência. No final, mostre uma lista de preços, organizando os dados em forma tabular;
+
+from time import sleep
+
 resposta = ''
 lista = []
 cont = 0
@@ -16,19 +19,23 @@ while resposta != 'N':
 
     item = (produto, valor)
     lista.append(item)
+
     soma += valor
     cont += 1
+
 print('')
 print('LISTAGEM DE PREÇOS')
 print('_'*30)
+print('carregando...')
+sleep(3)
 
 for item in lista:
 
     produto = item[0]
     valor = item[1]
 
-print(f'Você adicionou {cont} produtos na sacola')
     print(f'{produto}.............{valor}')
 print(f'Total {soma}')
+print(f'Você adicionou {cont} produtos na sacola')
 
 print('_'*30)
