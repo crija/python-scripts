538894ef09e37f8161ebaa1de5b3023cb2137f58
crija <crijacarla@gmail.com>
2023-04-27 16:31:21 -0300

    Add list

---
+++ mun3_listas.py
@@ -0,0 +1,7 @@
+valores = []
+valores.append(5)
+valores.append(4)
+valores.append(8)
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final da lista.')
