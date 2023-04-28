2c87136c84251e033dddf15aa8a5de27dc387035
crija <crijacarla@gmail.com>
2023-04-28 14:22:33 -0300

    Add modified

--- mun3_listas.py
+++ mun3_listas.py
@@ -1,7 +1,6 @@
 valores = []
-valores.append(5)
-valores.append(4)
-valores.append(8)
+for cont in range(0,5):
+    valores.append(int(input('Digite um valor: ')))
 for c, v in enumerate(valores):
     print(f'Na posição {c} encontrei o valor {v}!')
 print('Cheguei ao final da lista.')
