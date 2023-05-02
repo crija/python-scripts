160dd0774c4e13a65d01e219a44b5b4d1c699ec5
crija <crijacarla@gmail.com>
2023-05-01 22:39:36 -0300

    Add organization

--- mun3_listas.py
+++ mun3_listas.py
@@ -1,6 +1,11 @@
 valores = []
+
 for cont in range(0,5):
+
     valores.append(int(input('Digite um valor: ')))
+
 for c, v in enumerate(valores):
+
     print(f'Na posição {c} encontrei o valor {v}!')
+    
 print('Cheguei ao final da lista.')
