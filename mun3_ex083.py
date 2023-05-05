0efa93d0d19d7e3d5968ac1c00d0141b427c9ed9
crija <crijacarla@gmail.com>
2023-05-05 18:52:03 -0300

    Add non-repeating sorted list

--- mun3_ex083.py
+++ mun3_ex083.py
@@ -2,7 +2,22 @@
 #cadastre-os em uma lista,já na posiçao correta de inserção (sem usar o sort())
 #No final, mostre a lista ordenada na tela
 
-list()
-for n in range(1, 6):
+lista = []
+
+for c in range(0, 5):
+
     n = int(input('Digite um valor: '))
-    
+
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+            pos += 1
+
+print('-'*40)
+
+print(f'Os números digitados foram {lista}')
