46ff2cfde9d1ec770716fc02319f4bc160eaaaa1
crija <crijacarla@gmail.com>
2023-05-05 16:54:39 -0300

    Rename

---
+++ mun3_ex083.py
@@ -0,0 +1,8 @@
+#Crie um programa onde o usuário possa digitar cinco valores numéricos
+#cadastre-os em uma lista,já na posiçao correta de inserção (sem usar o sort())
+#No final, mostre a lista ordenada na tela
+
+list()
+for n in range(1, 6):
+    n = int(input('Digite um valor: '))
+    
