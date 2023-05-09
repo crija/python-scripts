9dbc47d4f5e1c3a1bb3482fe293e8519cdb38bd1
crija <crijacarla@gmail.com>
2023-05-09 17:02:22 -0300

    Add new version

--- listadafazer.py
+++ listadafazer.py
@@ -1,17 +1,32 @@
 #Criar uma lista de tarefas;
+from time import sleep
 tarefas = []
 tar = res = eliminar = rem = ''
+titulo = 'TAREFAS'
 
 while tar != 'n':
     tarefas.append(str(input('Digite a tarefa: ')))
     tar = str(input('Deseja adicionar mais alguma tarefa?[s/n] ')).lower()[0]
     if tar != 's':
+        print('-'*30)
+        print(titulo.center(30, '*'))
         for c, t in enumerate(tarefas):
             print(f'Tarefa n{c}: {t}')
-        print('Essas são as tarefas do dia')
+        print('-'*30)
         rem = str(input('Deseja remover alguma tarefa?[s/n] '))
         if rem == 's':
             elemento = int(input('Digite o número da tarefa: '))
             del tarefas[elemento]
+
+            print('carregando...')
+            sleep(3)
+
+            print('Lista alterada com sucesso!')
+            print('-'*30)
+            print(titulo.center(30, '*'))
             for i, o in enumerate(tarefas):
                 print(f'Tarefa n{i}: {o}')
+            print('-'*30)
+            break
+        else:
+            print('Programa encerrado')
