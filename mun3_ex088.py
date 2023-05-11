eeefcf102c5ee8de7d3ea94b92b5795bbaddacc8
crija <crijacarla@gmail.com>
2023-05-11 17:17:24 -0300

    Add new exercise

---
+++ mun3_ex088.py
@@ -0,0 +1,3 @@
+#Crie um programa onde o usuário possa digitar sete valores númericos
+# e cadastre-os em uma lista única que mantenha separados os valores
+#No final, mostre os valores pares e impares em ordem cresecente
