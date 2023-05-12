de71aee9bbf3bfbd36dd92d737955d5a2831e262
crija <crijacarla@gmail.com>
2023-05-12 17:09:26 -0300

    Add new version

--- mun3_ex088.py
+++ mun3_ex088.py
@@ -2,28 +2,17 @@
 # e cadastre-os em uma lista única que mantenha separados os valores
 #No final, mostre os valores pares e impares em ordem cresecente
 
+val = [[], []]
 v = 0
-val = []
-par = []
-imp = []
 
 for v in range(0, 7):
-    v = int(input('Digite um valor: '))
-    print('*')
+    v = int(input(f'Digite o {v}o número: '))
     if v % 2 == 0:
-
-        par.append(v)
-
+        val[0].append(v)
     else:
-        imp.append(v)
+        val[1].append(v)
+val[0].sort()
+val[1].sort()
 
-par.sort()
-imp.sort()
-val.append(par[:])
-val.append(imp[:])
-
-print('*'*53)
-print(f'Os números digitados foram: \033[2;31m{val}\033[m')
-print(f'Os números pares são: \033[2;31m{par}\033[m')
-print(f'Os números ímpares são: \033[2;31m{imp}\033[m')
-print('*'*53)
+print(f'Os números pares digitos foram: {val[0]}')
+print(f'Os números impares digitados foram: {val[1]}')
