38e2d31ff5ba9a91b035ca4ea2ca7dc4014454b0
crija <crijacarla@gmail.com>
2023-05-19 18:19:08 -0300

    Add script under development

---
+++ mun3_ex091.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário
+#No final, mostre o conteúdo da estrutura na tela
+
+aluno = []
+nome['n'] = str(input('Digite seu nome: '))
+aluno.append(nome.copy())
+print(aluno)
