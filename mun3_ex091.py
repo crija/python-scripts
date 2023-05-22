198ec0ceb3615e40240f03fe1801e1a4c0b12221
crija <crijacarla@gmail.com>
2023-05-22 14:38:56 -0300

    Add dictionary in python

--- mun3_ex091.py
+++ mun3_ex091.py
@@ -1,7 +1,17 @@
 #Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário
 #No final, mostre o conteúdo da estrutura na tela
 
-aluno = []
-nome['n'] = str(input('Digite seu nome: '))
-aluno.append(nome.copy())
-print(aluno)
+
+aluno = {}
+
+aluno['nome']= str(input('Nome: '))
+
+aluno['media']= float(input(f'Média de {aluno["nome"]}: '))
+
+if aluno['media'] < 6:
+    print(f'Aluno {aluno["nome"]} está reprovado')
+    print(f'Média: {aluno["media"]}')
+
+else:
+    print(f'Aluno(a) {aluno["nome"]} está aprovado(a)')
+    print(f'Média: {aluno["media"]}')
