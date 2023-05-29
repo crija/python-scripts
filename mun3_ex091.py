98176a709883fdc72af123a5674844746edf8f95
crija <crijacarla@gmail.com>
2023-05-29 15:22:39 -0300

    Add new file

--- mun3_ex091.py
+++ mun3_ex091.py
@@ -1,18 +1,14 @@
 #Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário
 #No final, mostre o conteúdo da estrutura na tela
 
+nome = str(input('Nome: '))
+nota = float(input('Nota: '))
+if nota < 6:
+aluno = {nome: nota}
+print(f'{aluno}')
 
-aluno = {}
 
-aluno['nome']= str(input('Nome: '))
 
-aluno['media']= float(input(f'Média de {aluno["nome"]}: '))
 
-if aluno['media'] < 6:
-    aluno['situação'] = 'Reprovado'
 
-else:
-    aluno['situação'] = 'Aprovado'
 
-for k, v in aluno.items():
-    print(f'{k} é igual a {v}')
