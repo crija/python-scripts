822e3008cc4dd1d8a748c3ab1dd4a40967fbfed5
crija <crijacarla@gmail.com>
2023-05-31 19:53:21 -0300

    Rename

---
+++ mun3_ex093.py
@@ -0,0 +1,10 @@
+#update previous file.  
+
+import random import randint
+
+players = {'player1': randint(1, 6),
+            'player2': randint(1, 6),
+            'player3': randint(1, 6),
+            'player4': randint(1, 6)
+}
+print(players)
