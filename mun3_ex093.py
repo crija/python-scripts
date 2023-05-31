81121898618d2063f7420593541daf2b863aa14a
crija <crijacarla@gmail.com>
2023-05-31 19:56:40 -0300

    Add new version of previous file

--- mun3_ex093.py
+++ mun3_ex093.py
@@ -1,10 +1,31 @@
 #update previous file.  
 
-import random import randint
+from random import randint
+from time import sleep
+from operator import itemgetter
 
-players = {'player1': randint(1, 6),
+
+players = {
+            'player1': randint(1, 6),
             'player2': randint(1, 6),
             'player3': randint(1, 6),
             'player4': randint(1, 6)
 }
-print(players)
+
+ranking = []
+
+for k, v in players.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+
+ranking = sorted(players.items(), key=itemgetter(1), reverse=True)
+#organizar a tabela de vencedores(formato decrescente do dicionario)
+
+print('')
+print('=-'*6)
+
+print('**RANKING**')
+for k, v in ranking:
+    print(f' {k}: {v}')
+
+print('=-'*6)
