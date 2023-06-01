c982e5c874dcd2f8f132128c9935dab0be7dd3bf
crija <crijacarla@gmail.com>
2023-06-01 14:30:09 -0300

    Add modigied

--- mun3_ex092.py
+++ mun3_ex092.py
@@ -6,19 +6,9 @@ from time import sleep
 
 organizar = []
 
-print('JOGADA')
 jogadores = {'Jogador1', 'Jogador2', 'Jogador3', 'Jogador4'}
 
 
 for c in jogadores:
     print(f'{c}: {random.randint(1, 7)}')
     sleep(2)
-
-print('RANKING')
-organizar.append(jogadores)
-
-organizar.sort()
-
-for i in range(1, 5):
-    if Jogador1 < Jogador2:
-        print(f'{1}:{jogador1}')
