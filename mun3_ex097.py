6b897bfd25c6d99ddb528186ea3d4aa0c1fee1d6
crija <crijacarla@gmail.com>
2023-06-08 17:00:59 -0300

    Add new name

--- mun3_ex097.py
+++ mun3_ex097.py
@@ -1,3 +1,14 @@
-#Faça um programa que tenha uma função chamada escreva();
-#Que receba um texto quelquer como parametro e mostre uma mensagem com tamanho adaptável.
+#Faça um programa que tenha uma função chamada área();
+#que receba as dimensões de um terreno retangular(largura e comprimento);
+#mostre a área do terreno.
 
+def area(largura, comprimento):
+    a = largura * comprimento
+    print(f'A área de um terreno {largura}x{comprimento} é de {a}m2')
+
+#Programa principal:
+print('Controle de terrenos')
+print('*' *20)
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+area(l,c)
