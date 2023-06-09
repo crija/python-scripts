0d56186994a2f6254227d3cfdabcd80af253e76c
crija <crijacarla@gmail.com>
2023-06-08 21:42:53 -0300

    Add modification

--- mun3_ex098.py
+++ mun3_ex098.py
@@ -1,9 +1,11 @@
 #Faça um programa que tenha uma função chamada escreva();
 #Que receba um texto quelquer como parametro e mostre uma mensagem com tamanho adaptável.
 
-def escreva():
-    print(len('Olá, mundo!')*'-')
+def escreva(msg):
+    print(len(msg)*'-')
+    print(msg)
+    print(len(msg)*'-')
 
-escreva()
-print('ola, mundo')
-escreva()
+
+escreva('Ola')
+escreva('mundo')
