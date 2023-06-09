f5fd602ce67e178f6121e6487bd40f26175e6198
crija <crijacarla@gmail.com>
2023-06-08 21:25:20 -0300

    Add print using function

--- mun3_ex098.py
+++ mun3_ex098.py
@@ -1,3 +1,9 @@
 #Faça um programa que tenha uma função chamada escreva();
 #Que receba um texto quelquer como parametro e mostre uma mensagem com tamanho adaptável.
 
+def escreva():
+    print(len('Olá, mundo!')*'-')
+
+escreva()
+print('ola, mundo')
+escreva()
