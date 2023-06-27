6e598d86000efa1200d1b247363c48b8bba11cee
crija <crijacarla@gmail.com>
2023-06-27 10:15:37 -0300

    Add ex2

---
+++ funcoes_part2_exemplo2.py
@@ -0,0 +1,18 @@
+'''EXEMPLO 2'''
+#Retornar False ou True:
+
+#Função com o parâmetro 'n' recebendo '0', como padrão.
+def par (n=0):
+
+#Condição de retorno.
+    if n % 2 = 0:
+        return True 
+    else:
+        return False
+    
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('Não é par!')
+
