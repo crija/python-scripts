c04ac2af9f737f4fd710b4b5c455056de3d3715e
crija <crijacarla@gmail.com>
2023-06-28 16:37:43 -0300

    Add update

--- mun3_ex102.py
+++ mun3_ex102.py
@@ -1,32 +1,19 @@
 #Crie um programa que tenha uma função chamada voto()que vai receber como parâmetro o ano de nascimento de uma pessoa
 #retornandoum valor literal indicando se uma pessoa tem voto NEGATIVO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 
-import datetime
-
 def voto(ano=0):
-    
-    if s < 18:
-        return 'Não é obrigatório'
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 18:
+        return f'Com {idade} o voto não é negado'
 
-    elif s - ano < 64 :
-        return 'É obrigatório'
+    elif idade - ano < 64 :
+        return f'Com {idade} o voto é obrigatório'
     
-    elif s - ano >= 65:
-        return 'Opcional'
+    elif idade - ano >= 65:
+        return f'Com {idade} o voto é opcional'
 
 #programa principal
-ano = int(input('Ano de nascimento: '))
-
-s = 2023 - ano
-
-if voto(ano)=='Não é obrigatório':
-    print(f'Com {s} anos o voto não é obrigatório')
-
-elif voto(ano)=='É obrigatório':
-    print(f'Com {s} anos o voto é obrigatório')
-
-elif voto(ano)=='Opcional':
-    print(f'Com {s} anos o voto é opcional')
-
-
-
+usuario = int(input('Qual ano você nasceu? '))
+print(voto(usuario))
