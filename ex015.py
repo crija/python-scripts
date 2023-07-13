fdaf167d42474d43e09b768b238019261c5d84dc
crija <crijacarla@gmail.com>
2023-07-12 22:07:50 -0300

    Add while and def

--- ex015.py
+++ ex015.py
@@ -1,78 +1,70 @@
  #Faça um algoritimo que leia o preço de um produto e mostre seu novo preço, com 70% de desconto:
-print('')
+produtos = {'Cadeira gamer': 'R$4.999', 'Sofá 4 lugares': 'R$3.578', 'Cama com baú' : 'R$2.670', 'Geladeira': 'R$5.000', 'Mesa de jantar 6 lugares': 'R$4.899'  }
+
+def lin():
+    print('**********************************')
+
+def espaco():
+    print()
+
+espaco()
 print('PARABÉNS, VOCẼ ACABA DE GANHAR 70% DE DESCONTO EM QUALQUER PRODUTO DA LISTA ABAIXO!')
+espaco()
+lin()
+espaco()
 
-print('\n')
+for k, v in produtos.items():
+    print(f'{k}: {v}')
 
-print('**********************************')
+espaco()
+lin()
+espaco()
 
-print('\n')
 
-print('Cadeira gamer: R$4.999')
-print('Sofá 4 lugares: R$3.578')
-print('Cama com baú: R$2.670')
-print('Geladeira: R$5.000')
-print('Mesa de jantar 6 lugares: R$4.899')
-
-print('\n')
-
-print('**********************************')
-
-print('\n')
-
-valor = float(input('Digite um dos valores a cima: R$'))
-
-print('\n')
-
-if valor != 4.999 and valor != 3.578 and valor != 2.670 and valor !=5.000 and valor != 4.899:
-    print('Valor não encontrado, tente novamente!')
+valor = 0
+while valor != 4.999 and valor != 3.578 and valor != 2.670 and valor !=5.000 and valor != 4.899:
+    valor = float(input('Digite um dos valores a cima: R$'))
 else:
     desconto = valor - (valor * 70 /100)
 
     print('( De R${:.3f} por R${:.3f} )'.format(valor, desconto))
-
-    print('')
-
+    espaco()
     print('Para que o desconto seja efetuado, cadastre-se agora mesmo!')
-
-    print('')
+    espaco()
 
     print('INFORMAÇÕES:')
 
-    print('')
-    print('_' *30)
-    print('nome*')
-    nome = input('- ')
-
-    if nome == '':
-        print('Campo obrigatório ^')
+    espaco()
+    name = ''
+    while name == '':
+        print('_' *30)
+        name = str(input('Digite seu nome: '))
+        if name == '':
+            print('Campo obrigatório ^')
 
     else:
-        print('e-mail*')
-        gmail = input('- ')
-
-        if gmail == '':
-            print('Campo obrigatório ^')
-        else:
-            print('Idade*')
-            idade = int(input('- '))
+        email = ''
+        while email == '':
+            email = input('Digite seu email:')
+            if email == '':
+                print('Campo obrigatório ^')
+                
+        else:            
+            idade = int(input('Digite sua idade: '))
 
             print('_' *30)
 
             if idade < 17:
                 print('Cadastro não disponível para menores de 18 anos')
+
             else:
-                print('')
-
-                print('nome:{}, gmail:{} , idade:{} anos.'.format(nome, gmail, idade))
-
-                print('')
-
+                espaco()
+                print('nome:{}, gmail:{} , idade:{} anos.'.format(name, email, idade))
+                espaco()
                 print('SEGURANÇA:')
 
                 print('Para manter sua conta segura siga as instruções abaixo.')
-
-                print('')
+                espaco()
 
                 nome = str(input('Digite apenas seu primeiro nome: '))
 
