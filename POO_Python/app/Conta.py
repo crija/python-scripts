73e1ab3e455e916bfb6520ccbc68fdfa4b27204f
crija <crijacarla@gmail.com>
2023-08-07 15:47:33 -0300

    Add get and set

--- POO_Python/app/Conta.py
+++ POO_Python/app/Conta.py
@@ -4,4 +4,13 @@ class Conta:
         self.saldo = 0
         self.numero = numero
         self.titular = titular
+
+        def get_saldo(self):
+            return self._saldo
+        
+        def set_saldo(self, saldo):
+            if (saldo<0):
+                print('O saldo não pode ser negativo')
+            else:
+                self._saldo = saldo
         
