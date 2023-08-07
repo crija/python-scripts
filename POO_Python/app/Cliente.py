fe57ddc6c9e3542a0808b0ce7b7c004e65f033ba
crija <crijacarla@gmail.com>
2023-08-07 15:46:56 -0300

    Add get and set

--- POO_Python/app/Cliente.py
+++ POO_Python/app/Cliente.py
@@ -3,3 +3,11 @@ class Cliente:
         
         self._nome = n
         self._telefone = fone
+
+    #método get
+    def get_nome(self):
+        return self._nome
+
+    #método set
+    def set_nome(self, nome):
+        self._nome = nome
