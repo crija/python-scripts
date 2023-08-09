560ebbebcf54a77fb99dba6ddff0c6b55a9a42f7
crija <crijacarla@gmail.com>
2023-08-09 15:47:03 -0300

    Add method

--- POO_Python/teste.py
+++ POO_Python/teste.py
@@ -1,19 +1,14 @@
 class Pessoa:
-    def __init__(self, nome, idade, sexo):
+    def __init__(self, nome, idade, sexo, ativo):
         self.nome = nome
         self.idade = idade
         self.sexo = sexo
 
 
-pessoa1 = Pessoa('Eduardo', 27, 'M')
-print(f'1 nome: {pessoa1.nome}, idade: {pessoa1.idade}, sexo: {pessoa1.sexo}')
+    def desativar(self):
+        self.ativo = False
+        print(f'{pessoa1} foi desafivada com sucesso')
+        
+pessoa1 = Pessoa('Eduardo', 27, 'M', True)
+pessoa1.desativar()
 
-pessoa1.nome = 'Claudio'
-pessoa1.idade = 34
-
-print(f'2 nome: {pessoa1.nome}, idade: {pessoa1.idade}, sexo: {pessoa1.sexo}')
-
-pessoa2 = Pessoa('Bia', 12, 'F')
-print(f'3 nome: {pessoa2.nome}, idade: {pessoa2.nome}, sexo: {pessoa2.sexo}')
-
-print(Pessoa)
