38cfbf29ceed87bf9d33a7bee989c1b2ef74623c
crija <crijacarla@gmail.com>
2023-08-21 19:20:40 -0300

    Add error handling with except and finally

--- mun3_ex107.py
+++ mun3_ex107.py
@@ -4,6 +4,13 @@ try:
     a = int (input('Numerador: '))
     b = int(input('Denumerador: '))
     r = a/b
-except:
-    print('Ocorreu um erro :(')
-print(f'O resultado é: {r}')
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou.')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero!')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados!')
+else:
+    print(f'O resultado é {r:.2}')
+finally:
+    print('Volte sempre!')
