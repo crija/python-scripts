221f4d2294c26c8189b6ec2b3a094a7f5d2c9ffa
crija <crijacarla@gmail.com>
2023-08-22 16:13:03 -0300

    Update with sleep, while, if and lower

--- gerando_senha_aleatoria.py
+++ gerando_senha_aleatoria.py
@@ -1,10 +1,35 @@
 from random import choice
 import string
+from time import sleep
 
-quantidade_caracter = int(input('Quantos caracteres a senha deve ter? '))
-caracteres = string.ascii_letters + string.digits + string.punctuation
-senha = ''
-for i in range(quantidade_caracter):
-    senha += choice(caracteres)
+while True:
 
-print(f'Sua senha é: {senha}')
+    print('*** Gerador de senhas aleatórias ***')
+    quantidade_caracter = 0
+
+    while quantidade_caracter < 4:
+        print('-'*53)
+        print('Por questão de segurança escolha no mínimo 4 dígitos!')
+        quantidade_caracter = int(input('Escolha a quantidade de dígitos da sua senha: '))
+        print('*')
+
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''
+
+    for i in range(quantidade_caracter):
+        senha += choice(caracteres)
+
+    print('Sua senha está sendo gerada. Aguarde!')
+    sleep(3)
+    print(f'senha: {senha}')
+    print('Mantenha sua senha segura!')
+    print('-'*53)
+
+    escolha = str(input('Deseja gerar uma nova senha [sim/nao]? ')).lower()
+    if escolha == 'sim':
+        print('Aguarde...')
+        sleep(2)
+        print('')
+
+    else:
+        break
