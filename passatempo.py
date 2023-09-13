c8d64f2ae1090e44f914e1e77faeca8e9e3b38bb
crija <crijacarla@gmail.com>
2023-09-13 16:24:53 -0300

    Update

--- passatempo.py
+++ passatempo.py
@@ -1,4 +1,5 @@
-projetos = []
+projetos_iniciados = []
+projetos_finalizados =[]
 
 def linha():
     print('-'*30)
@@ -6,14 +7,25 @@ def linha():
 print('MANTENHA SEUS PROJETOS ORGANIZADOS')
 linha()
 while True:
+    print('*')
     nome_projeto = str(input('Digite o nome do projeto:'))
+    escolha = str(input('O projeto foi finalizado? '))
+    if escolha == 's':
+        projetos_finalizados.append(nome_projeto)
+    elif escolha == 'n':
+        projetos_iniciados.append(nome_projeto)
     res = str(input('Deseja adicionar mais algum projeto? [s/n]: ')).lower()
-    projetos.append(nome_projeto)
     if res == 'n':
         break
 linha()
-print('Projetos não terminados')
-for count, value in enumerate(projetos):
+print(f'Você tem {len(projetos_iniciados)} projetos esperando por você :)')
+for count, value in enumerate(projetos_iniciados):
     print(count+1, value)
-print(f'Você tem {len(projetos)} projetos esperando por você :)')
+    
+linha()
+print(f'Parabéns! Você já tem {len(projetos_finalizados)} projetos finalizados :)')
+for c, v in enumerate(projetos_finalizados):
+    print(c+1, v)
+
+
 
