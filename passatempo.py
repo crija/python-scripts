b573858fc163d96624d494c6ebca8e6ddc13481a
crija <crijacarla@gmail.com>
2023-09-23 15:33:22 -0300

    Add conditional

--- passatempo.py
+++ passatempo.py
@@ -10,22 +10,35 @@ while True:
     print('*')
     nome_projeto = str(input('Digite o nome do projeto:'))
     escolha = str(input('O projeto foi finalizado? '))
+
     if escolha == 's':
         projetos_finalizados.append(nome_projeto)
+
     elif escolha == 'n':
         projetos_iniciados.append(nome_projeto)
     res = str(input('Deseja adicionar mais algum projeto? [s/n]: ')).lower()
+
     if res == 'n':
         break
+
+fin = len(projetos_finalizados)
+ini = len(projetos_iniciados)
+
 linha()
-print(f'Você tem {len(projetos_iniciados)} projetos esperando por você :)')
-for count, value in enumerate(projetos_iniciados):
-    print(count+1, value)
-    
+
+if ini > 0:
+    print(f'Você tem {len(projetos_iniciados)} projetos esperando por você :)')
+    for count, value in enumerate(projetos_iniciados):
+        print(count+1, value)
+else:
+    print('Você não tem projetos em andamento')
+
 linha()
-print(f'Parabéns! Você já tem {len(projetos_finalizados)} projetos finalizados :)')
-for c, v in enumerate(projetos_finalizados):
-    print(c+1, v)
 
+if fin > 0:
+    print(f'Parabéns! Você já tem {len(projetos_finalizados)} projetos finalizados :)')
+    for c, v in enumerate(projetos_finalizados):
+        print(c+1, v)
 
-
+else:
+    print('Você não tem projetos finalizados')
