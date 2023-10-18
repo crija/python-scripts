f875194c7bf4ac841c542b09b339bc161df46b96
crija <crijacarla@gmail.com>
2023-10-18 16:01:33 -0300

    Add float

--- reforco_def.py
+++ reforco_def.py
@@ -1,9 +1,9 @@
 #Faça um programa que tenha uma função chamada área(), que receba as dimenções de um terreno retangular(largura e comprimento) e mostre a área do terreno.
 
 def area(larg, comp):
-    print(f'Largura: {larg}, Comprimento: {comp}: Área = {larg * comp}')
+    print(f'Largura: {larg}, Comprimento: { comp }: Área ={larg * comp :.2f}')
 
 #programa principal
-largura = int(input('Largura: '))
-comprimento = int(input('Comprimento: '))
+largura = float(input('Largura: '))
+comprimento = float(input('Comprimento: '))
 area(largura, comprimento)
