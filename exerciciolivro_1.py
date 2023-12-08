980949463d5c04969c0a2fd3dbac03551a26f974
crija <crijacarla@gmail.com>
2023-12-08 12:21:42 -0300

    Add exercise 1 from the book python intensive course

---
+++ exerciciolivro_1.py
@@ -0,0 +1,12 @@
+sandwich_orders = ['atum', 'carne', 'frango', 'salada', 'presunto']
+finished_sandwich = []
+
+for sandwite in sandwich_orders:
+    print(f'Seu lanche {sandwite} está pronto.')
+    finished_sandwich.append(sandwite)
+
+for valor in enumerate(finished_sandwich):
+    print(valor)
+
+
+
