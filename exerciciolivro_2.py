a265e0cd50c4b477d55549dff75c9af5ce56295e
crija <crijacarla@gmail.com>
2023-12-22 00:11:10 -0300

    Add size and additional

--- exerciciolivro_2.py
+++ exerciciolivro_2.py
@@ -1,13 +1,37 @@
-igredientes = []
+tamanho = {'P:': '4 fatias', 'M:': '8 fatias', 'G:': '12 fatias'}
+igredientes_dis = ['queijo', 'calabresa', 'carne', 'frango', 'brócolis', 'atum', 'cebola', 'presunto', 'molho vermalho']
 adicional = {1:'cheddar' , 2:'maionese verde', 3:'batata palha', 4:'milho verde'}
-adicionais_elcolhidos = []
+adicionais_escolhidos = []
 
-sair = ''
-while sair != 'quit':
+def igredientes():
+    print('Igredientes Disponíveis')
+    for i in igredientes_dis:
+        print(i)
+
+print('Tamanhos')
+for t, f in tamanho.items():
+    print(t, f)
+
+escolher_tamanho = str(input('Digite o tamanho escolhido: ')).upper()
+
+match escolher_tamanho:
+    case 'P':
+        igredientes()
+        p = str(input('Escolha até 3 iguedientes: '))
+    case 'M':
+        igredientes()
+        m = str(input('Escolha até 4 iguedientes: '))
+    case 'G':
+        igredientes()
+        g = str(input('Escolha até 5 igredientes: '))
+
+'''
+stop = ''
+while stop != 'skip':
     escolha = str(input('Adicione um igrediente na pizza: '))
     igredientes.append(escolha)
     print(f'{escolha} adicionado')
-    sair = str(input('Para sair digite quit: '))
+    stop = str(input('Para sair digite skip: '))
 
 igrediente_adicional = str(input('Igrediente adicional? ')).lower()
 if igrediente_adicional == 'sim':
@@ -17,7 +41,7 @@ if igrediente_adicional == 'sim':
 selecionar_adicional = ''
 while selecionar_adicional != 'nao':
     numero_adicional = int(input('Digite o número do adicional escolhido: '))
-    adicionais_elcolhidos.append(numero_adicional)
+    adicionais_escolhidos.append(numero_adicional)
     selecionar_adicional = str(input(f'Você escolheu {numero_adicional}. Escolher mais adicionais?[y or no] (Para sair digite sair.): ')).lower()
     if selecionar_adicional == 'sair':
         break
@@ -27,9 +51,9 @@ print('Igredientes adicionados')
 for i in igredientes:
     print(i)
 print('Número dos adicionais')
-for a in adicionais_elcolhidos:
+for a in adicionais_escolhidos:
     print(a)
 
-
+'''
 
 
