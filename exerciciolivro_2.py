2752b5a1d0729b1d3555ceeabb9879bc53e584d7
crija <crijacarla@gmail.com>
2023-12-28 11:50:07 -0300

    Part 4, add edge

--- exerciciolivro_2.py
+++ exerciciolivro_2.py
@@ -1,6 +1,7 @@
 pizza_sizes = {'P:': '4 fatias', 'M:': '8 fatias', 'G:': '12 fatias'}
 list_igredients = ['queijo', 'calabresa', 'carne', 'frango', 'brócolis', 'atum', 'cebola', 'presunto', 'molho vermelho']
 list_additionals = {1:'cheddar' , 2:'maionese verde', 3:'batata palha', 4:'milho verde'}
+list_edge = ['catupiry', 'cheddar', 'morango', 'chocolate branco', 'bombom']
 chosen_igredients = []
 chosen_additional = []
 
@@ -10,17 +11,29 @@ for t, f in pizza_sizes.items():
 
 choose_pizza_size = str(input('Digite o tamanho escolhido: ')).upper()
 
+print('Bordas')
+for b in list_edge:
+    print(b)
+
+print('Se preferir sem borda digite (sem)')
+add_edge = str(input('Escolha o recheio da borda: ')).lower()
+if add_edge == 'sem':
+    print('ok')
+
+
 if choose_pizza_size == 'P' or 'M' or 'G':
     print('Igredientes Disponíveis')
     for i in list_igredients:
         print(i)
 
-stop = ''
-while stop != 'skip':
+escolha = ''
+while escolha != 'skip':
     escolha = str(input('Adicione um igrediente na pizza: '))
+    if escolha == 'skip':
+        break
     chosen_igredients.append(escolha)
-    print(f'{escolha} adicionado')
-    stop = str(input('Para sair digite skip: '))
+
+
 
 igrediente_adicional = str(input('Igrediente adicional? [yes or no]')).lower()
 if igrediente_adicional == 'yes':
@@ -34,12 +47,17 @@ if igrediente_adicional == 'yes':
         if selecionar_adicional == 'no':
             break
 
+print('Tamanho')
+print(f'{choose_pizza_size}')
+print('Borda')
+print(f'{add_edge}')
 print('Igredientes adicionados')
 for i in chosen_igredients:
     print(i)
 print('Número dos adicionais')
 for a in chosen_additional:
     print(a)
+
 
 
 
