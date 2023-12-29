0ae972693ef98469ea18216fa7ce1a4e1b0b3814
crija <crijacarla@gmail.com>
2023-12-29 15:50:20 -0300

    Part 5, add pizza value

--- exerciciolivro_2.py
+++ exerciciolivro_2.py
@@ -1,4 +1,4 @@
-pizza_sizes = {'P:': '4 fatias', 'M:': '8 fatias', 'G:': '12 fatias'}
+pizza_sizes = {'P:': '4 fatias - R$34,99', 'M:': '8 fatias - R$42,99', 'G:': '12 fatias - R$55,99'}
 list_igredients = ['queijo', 'calabresa', 'carne', 'frango', 'brócolis', 'atum', 'cebola', 'presunto', 'molho vermelho']
 list_additionals = {1:'cheddar' , 2:'maionese verde', 3:'batata palha', 4:'milho verde'}
 list_edge = ['catupiry', 'cheddar', 'morango', 'chocolate branco', 'bombom']
