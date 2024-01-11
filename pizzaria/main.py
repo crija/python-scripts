0a07b20f0da5a130017fe800014fd130e4d3ab50
crija <crijacarla@gmail.com>
2024-01-11 17:20:46 -0300

    Modify ingredient names and add values

--- pizzaria/main.py
+++ pizzaria/main.py
@@ -1,9 +1,11 @@
 import csv
 from limiters import *
 
-pizza_sizes = {'P:': '4 fatias - R$34,99', 'M:': '8 fatias - R$42,99', 'G:': '12 fatias - R$55,99'}
-list_additionals = {1:'cheddar' , 2:'maionese verde', 3:'batata palha', 4:'milho verde'}
-list_edge = ['catupiry', 'cheddar', 'morango', 'chocolate branco', 'bombom']
+additionals_value = [4.25, 2.50, 5.15, 8.90,]
+edge_value = [10.99, 15.90]
+pizza_sizes = {'P:': '4 fatias', 'M:': '8 fatias', 'G:': '12 fatias'}
+list_additionals = ['cheddar', 'maionese verde', 'morango', 'chocolate preto']
+list_edge = ['catupiry', 'bombom']
 chosen_additional = []
 
 print('Tamanhos')
@@ -34,7 +36,7 @@ if igrediente_adicional == 'yes':
         print(k, v)
 
     while igrediente_adicional != 'no':
-        igrediente_adicional = int(input('Digite o número do adicional escolhido: '))
+        igrediente_adicional = str(input('Digite o nome do adicional escolhido: '))
         chosen_additional.append(igrediente_adicional)
         selecionar_adicional = str(input(f'Você escolheu {igrediente_adicional}. Escolher mais adicionais?[yes or no]: ')).lower()
         if selecionar_adicional == 'no':
@@ -49,7 +51,7 @@ with open("pizzaria/arquivo.csv", 'a', newline='') as arquivo:
 
     writer.writerow({
                         'tamanho': choose_pizza_size,
-                        'igredientes': chosen_igredients,
+                        'igredientes': chosen_flavor,
                         'borda': add_edge,
                         'adicional': chosen_additional
                     })
