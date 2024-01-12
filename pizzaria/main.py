cc961bd7d7afa4916369385dc0aaa9677cadd3ac
crija <crijacarla@gmail.com>
2024-01-12 19:18:13 -0300

    Add sum of values solution

--- pizzaria/main.py
+++ pizzaria/main.py
@@ -2,7 +2,7 @@ import csv
 from limiters import *
 
 additionals_value = [4.25, 2.50, 5.15, 8.90,]
-edge_value = [10.99, 15.90]
+edge_value = [10.99, 15.90, 0]
 pizza_sizes = {'P:': '4 fatias', 'M:': '8 fatias', 'G:': '12 fatias'}
 list_additionals = ['cheddar', 'maionese verde', 'morango', 'chocolate preto']
 list_edge = ['catupiry', 'bombom']
@@ -25,27 +25,48 @@ print('Bordas')
 for b in list_edge:
     print(b)
 
-print('Se preferir sem borda digite (sem)')
 add_edge = str(input('Escolha o recheio da borda: ')).lower()
-if add_edge == 'sem':
-    print('ok')
+
+if add_edge == list_edge[0]:
+    final_value.append(edge_value[0])
+elif add_edge == list_edge[1]:
+    final_value.append(edge_value[1])
+else:
+    final_value.append(edge_value[0])
 
 igrediente_adicional = str(input('Igrediente adicional? [yes or no]')).lower()
 if igrediente_adicional == 'yes':
-    for k, v in list_additionals.items():
-        print(k, v)
+    for i in list_additionals:
+        print(i)
 
     while igrediente_adicional != 'no':
-        igrediente_adicional = str(input('Digite o nome do adicional escolhido: '))
+        igrediente_adicional = str(input('Digite o nome do adicional escolhido: ')).lower()
         chosen_additional.append(igrediente_adicional)
+
+        match igrediente_adicional:
+            case 'cheddar':
+                final_value.append(additionals_value[0])
+            case 'maionese verde':
+                final_value.append(additionals_value[1])
+            case 'morango':
+                final_value.append(additionals_value[2])
+            case 'chocolate preto':
+                final_value.append(additionals_value[3])
+
         selecionar_adicional = str(input(f'Você escolheu {igrediente_adicional}. Escolher mais adicionais?[yes or no]: ')).lower()
+
+
+
+        print(final_value)
+        print(sum(final_value))
+
         if selecionar_adicional == 'no':
             break
-
 
 with open("pizzaria/arquivo.csv", 'a', newline='') as arquivo:
     campos_head = ['tamanho', 'igredientes', 'borda', 'adicional']
     writer = csv.DictWriter(arquivo, fieldnames=campos_head, delimiter=';')
+
     if arquivo.tell() == 0:
         writer.writeheader()
 
