5735128994fccb785dbe1ae5c55681bcc63fd72e
crija <crijacarla@gmail.com>
2024-03-12 18:06:15 -0300

    Add new finction

--- aprendendo_POO/ex002.py
+++ aprendendo_POO/ex002.py
@@ -1,5 +1,20 @@
-def soma(a, b):
+def armazenar(nome, idade, peso):
+    return nome, idade, peso
+
+dados = armazenar('Ana', 3, 17)
+print(dados)
+
+
+
+
+
+
+
+
+
+'''def soma(a, b):
     return a + b
 
 resultado = soma(3, 8)
 print(resultado)
+'''
