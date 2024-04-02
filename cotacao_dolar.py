187eb8194898d06fa6a183d98cbf6f4f0a925fd6
crija <crijacarla@gmail.com>
2024-04-02 16:02:23 -0300

    Add dollar rate

---
+++ cotacao_dolar.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+cotacoes = cotacoes.json()
+cotacoes_dolar = cotacoes["USDBRL"]["bid"]
+print(cotacoes_dolar)
