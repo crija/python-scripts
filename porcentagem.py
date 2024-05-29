2ce4b20d94b572c7e516c9fd90813abd5bd5b769
John Doe <crijacarla@gmail.com>
2024-05-29 13:05:52 -0300

    Removing unnecessary lines, switching to English and creating link

--- porcentagem.py
+++ porcentagem.py
@@ -1,15 +1,1 @@
-import sys
-print ('argument list', sys.argv)
-valor = sys.argv[1]
-desconto = sys.argv[2]
-
-
-valor = int(valor)
-desconto = int(desconto)
-print(valor, desconto)
-
-
-total_desconto = valor * desconto / 100
-total_pagar = valor - total_desconto
-print(f'desconto R${total_desconto}')
-print(f'total a pagar R${total_pagar}')
+/usr/local/bin/discount
