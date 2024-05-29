a09f197e4b317d52938c069776d3475f36c4b72d
John Doe <crijacarla@gmail.com>
2024-05-28 21:36:11 -0300

    Add total payable

--- porcentagem.py
+++ porcentagem.py
@@ -10,4 +10,6 @@ print(valor, desconto)
 
 
 total_desconto = valor * desconto / 100
+total_pagar = valor - total_desconto
 print(f'desconto R${total_desconto}')
+print(f'total a pagar R${total_pagar}')
